5e8b251c6b360271ddc05ace82d81a19191216c7
周木生 <zhoumusheng@zhoumushengdeMacBook-Pro.local>
2018-12-21 02:41:19 +0800

    fin

--- src/content.js
+++ src/content.js
@@ -1,163 +1,41 @@
 $(function() {
-  const triggerBtn = () => {
-    let btn = $('<button id="data_business">回填参谋数据</button>');
-    $("body").prepend(btn)
-  }
-  triggerBtn();
+  let _href = location.href;
+  if (_href.includes('page/datacount/sycm.html')) {
 
-  let href = window.location.href;
+    const triggerFn = () => {
+      let triggerBtn = $("#btn_business");
+
+      if (triggerBtn) {
+        triggerBtn.on("click", () => {
+          let shopId = $("#search_shopId").val();
+          let token = $("#token").val();
+          if (!shopId) {
+            alert('请先选择店铺');
+            return;
+          }
+          chrome.runtime.sendMessage({
+            greeting: "business",
+            tk: token,
+            shopId: shopId
+          });
+        })
+      }
+    }
+    triggerFn();
+  }
+  // const triggerBtn = () => {
+  //   let btn = $('<button id="btn_business">回填参谋数据</button>');
+  //   $("body").prepend(btn)
+  // }
+  // triggerBtn();
+
   // if (href.includes('login.html')) return;
 
-  const triggerFn = () => {
-    let triggerBtn = $("#data_business");
-    if (triggerBtn) {
-      triggerBtn.on("click", () => {
-        chrome.runtime.sendMessage({
-          greeting: "business",
-        });
-      })
-    }
-  }
-  triggerFn();
+
 
 
 
   return;
 
-  alert(555)
 
-  //监听版本更新信息
-  chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
-      if (request.greeting == "checkV" && request.res.verSeq > request.res.app_verSeq) {
-        $("#smartTip").attr({
-          'data-isForced': request.res.isForced
-        }).html(`${request.res.description},<a href='${request.res.url}' target="_blank">点击下载</a>`).show();
-        $("#reinstall-download-plug").attr({
-          "href": request.res.url
-        });
-      }
-    });
-
-
-
-  var checkPlugNode = document.createElement('div');
-  checkPlugNode.id = 'my-chrome-extension-installed';
-  checkPlugNode.style.display = 'none';
-  //    checkPlugNode.setAttribute('version', chrome.extension.getManifest().version); // 把版本号放到属性里
-  checkPlugNode.innerText = JSON.stringify({
-    key: 'value'
-  }); // 把通信的data放到标签的html text里面
-  document.body.appendChild(checkPlugNode);
-  var configData = null;
-  var tenMin = function() {
-    let tenMin = new Date().getTime() + 1000 * 600;
-    return new Date(parseInt(tenMin)).toLocaleString().substr(0, 17);
-    // console.log('tenMin',now,Date.parse(new Date()))
-  };
-
-
-  //登录成功后操作
-  var sucLoginBtn = document.getElementById('suc-login-tb'),
-    smartSetBtn = document.getElementById('smartSet'),
-    rollBackDataBtn = document.getElementById('rollBackData');
-
-  sucLoginBtn.addEventListener('eventByLogin', function() {
-    $.ajax({
-      url: 'https://sycm.taobao.com/custom/menu/getPersonalView.json',
-      async: false,
-      success: function(res) {
-        // let ten = tenMin();
-        if (res.code == 0) {
-          $(".window-panel").height(260)
-          $("#step-login").removeClass("step-current");
-          $("#step-data").addClass("step-current");
-          $(".window-panel").find(".title").find("h3").html('正在智能回填数据');
-          $("#s-step-tip-2").hide();
-          $("#s-step-tip-3").show();
-
-          let darenId = $('#smartSet').attr("data-darenid") || 10000,
-            darenNameCN = $('#smartSet').attr("data-darenname"),
-            darenName = encodeURIComponent(darenNameCN),
-            sid = $('#smartSet').attr("data-sid") || 10000,
-            tk = $('#smartSet').attr("data-token") || 10000;
-          $("#smartTip").html(`您即将要做智能回填的是达人“${darenNameCN}”，请确保当前创作平台登录的是该达人，否则智能回填无法生效。`).show();
-          setTimeout(function() {
-            $("#smartTip").hide();
-            $(".plug-step-win").hide();
-          }, 13 * 1000 * 60)
-          console.log("开始获取配置数据，请稍候。。。")
-          chrome.runtime.sendMessage({
-            greeting: "triggerConfig",
-            head: "tk=" + tk + "&darenId=" + darenId + "&darenName=" + darenName + "&sid=" + sid
-          }, function(response) {
-            // console.log('sucLoginBtn',response)
-            configData = response;
-            $("#rollBackData").trigger("click")
-          });
-        } else {
-          alert('未收到平台确认信息，请重新登录创作平台。');
-        }
-      }
-    })
-
-  })
-  //
-  smartSetBtn.addEventListener('eventBySmartSet', function() {
-    //从淘宝获取用户信息判断是否登录
-    $.ajax({
-      url: 'https://sycm.taobao.com/custom/menu/getPersonalView.json',
-      success: function(res) {
-        // let ten = tenMin();
-        if (res.code == 0) {
-          $(".window-panel").height(260)
-          $("#s-step-tip-1").hide();
-          $("#s-step-tip-2").hide();
-          $("#step-login").removeClass("step-current");
-          $("#step-data").addClass("step-current");
-          $(".window-panel").find(".title").find("h3").html('正在智能回填数据');
-          $("#s-step-tip-3").show();
-
-          let darenId = $('#smartSet').attr("data-darenid") || 10000,
-            darenNameCN = $('#smartSet').attr("data-darenname"),
-            darenName = encodeURIComponent(darenNameCN),
-            sid = $('#smartSet').attr("data-sid") || 10000,
-            tk = $('#smartSet').attr("data-token") || 10000;
-          $("#smartTip").html(`您即将要做智能回填的是达人“${darenNameCN}”，请确保当前创作平台登录的是该达人，否则智能回填无法生效。`).show();
-          setTimeout(function() {
-            $("#smartTip").hide();
-            $(".plug-step-win").hide();
-          }, 13 * 1000 * 60)
-          console.log("开始获取配置数据，请稍候。。。")
-          chrome.runtime.sendMessage({
-            greeting: "triggerConfig",
-            head: "tk=" + tk + "&darenId=" + darenId + "&darenName=" + darenName + "&sid=" + sid
-          }, function(response) {
-            configData = response;
-            $("#rollBackData").trigger("click");
-          });
-        }
-      }
-    })
-  });
-  rollBackDataBtn.addEventListener('eventByRollBackData', function() {
-    let darenId = $('#smartSet').attr("data-darenid") || 10000,
-      darenName = encodeURIComponent($('#smartSet').attr("data-darenname") || 10000),
-      sid = $('#smartSet').attr("data-sid") || 10000,
-      tk = $('#smartSet').attr("data-token") || 10000;
-    let headObj = {
-      darenId: darenId,
-      tk: tk,
-      darenName: darenName,
-      sid: sid
-    };
-    console.log('Begin post data....', configData);
-
-    //爬数据
-    chrome.runtime.sendMessage({
-      greeting: "spider",
-      interFaceList: configData.result,
-      head: headObj
-    });
-  });
 })
--- src/eventPage.js
+++ src/eventPage.js
@@ -68,11 +68,11 @@ const checkLogin = () => {
     })
   })
 }
-const getDataFromSycm = (argumentes) => {
+const getDataFromSycm = (contentId, dateFrom, dateTo) => {
   let tk = util.generatTk(9);
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: `https://sycm.taobao.com/xsite/content/single/detail/result.json?dateType=recent30&dateRange=2018-10-10%7C2018-11-08&indexCode=&contentId=207962459196&_=1541740203285&token=${tk}`,
+      url: `https://sycm.taobao.com/xsite/content/single/detail/result.json?dateType=range&dateRange=${dateFrom}%7C${dateTo}&indexCode=&contentId=${contentId}&_=1541740203285&token=${tk}`,
       success(res) {
         if (!res.hasError) {
           resolve(res.content.data)
@@ -87,7 +87,8 @@ const getDataFromSycm = (argumentes) => {
   })
 }
 let getContentIdPages = 1; //取ContentId时的总页数
-const getContentId = (shopId, loginUserId, mainUserId, page = 1, pageSize = 20) => {
+let getContentIdLength = 0; //取ContentId时的总页数
+const getContentId = (shopId, loginUserId, mainUserId, token, page = 1, pageSize = 50) => {
   let beginDay = getDateRange(10);
   let endDay = getDateRange(2);
   let contenId = [];
@@ -95,9 +96,9 @@ const getContentId = (shopId, loginUserId, mainUserId, page = 1, pageSize = 20) 
     url: `${moli_host}/mer/getContentIdsByShop.wb`,
     async: false,
     headers: {
-      version: '1.1.4',
+      version: '1.0.0',
       vs: '1',
-      tk: '0000000000000'
+      tk: token
     },
     data: {
       page,
@@ -107,19 +108,30 @@ const getContentId = (shopId, loginUserId, mainUserId, page = 1, pageSize = 20) 
       mainUserId
     },
     success(res) {
-      console.log('getContentId', res)
-      if (!res.hasError) {
-        getContentIdPages = Math.ceil(res.content.data.recordCount / pageSize);
-        res.content.data.data.forEach((item, key) => {
-          contenId = [...contenId, item.contentId]
+      if (res.status === 0) {
+        // getDataFromSycm
+        // conso
+        getContentIdLength = res.result.contetnIds.length
+        res.result.contetnIds.forEach((item, key) => {
+          getDataFromSycm(item, res.result.dateFrom, res.result.dateTo).then((data) => {
+            postContentData(loginUserId, mainUserId, data, token)
+          })
         })
+        // contenId = [...contenId, ...res.result.contetnIds];
+        // getContentIdLength = res.result.contetnIds.length
+
+        // getContentIdPages = Math.ceil(res.result.data.recordCount / pageSize);
+        // res.content.data.data.forEach((item, key) => {
+        //   contenId = [...contenId, item.contentId]
+        // })
+      } else {
+        getContentIdLength = 0
       }
     }
   })
   return contenId;
 }
-
-const postShopData = (param) => {
+const postShopData = (param, token) => {
   return new Promise((resolve, reject) => {
     $.ajax({
       url: `${moli_host}/mer/syncPersonalInfo.wb`,
@@ -127,7 +139,7 @@ const postShopData = (param) => {
       headers: {
         version: '1.1.4',
         vs: '1',
-        tk: '0000000000000'
+        tk: token
       },
       data: param,
       success(response) {
@@ -141,10 +153,34 @@ const postShopData = (param) => {
     })
   })
 }
+const postContentData = (loginUserId, mainUserId, dataList, token) => {
+  let _itemList = [];
+  _itemList.push(dataList)
+  $.ajax({
+    url: `${moli_host}/mer/syncMerEffect.wb`,
+    contentType: 'application/json',
+    headers: {
+      version: '1.0.0',
+      vs: '1',
+      tk: token,
+    },
+    type: 'post',
+    data: JSON.stringify({
+      loginUserId,
+      mainUserId,
+      itemList: _itemList
+    }),
+    success(res) {
+      console.log(res);
+    }
+  })
+}
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
   if (request.greeting == 'business') {
+    let _tk = request.tk;
+    let shopId = request.shopId;
     //检查参谋是否登录，未登录弹出提示，已登录开始爬数据
     checkLogin().then((res) => {
       // let colectContentIdByPage = []; //收集每个分页的congentid；
@@ -162,355 +198,60 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       // }
       loginUserId = res.loginUserId;
       mainUserId = res.mainUserId;
-      console.log('colectContentIdByPage', res)
 
-      postShopData(res).then((res) => {
+      postShopData(res, _tk).then((res) => {
         let colectContentIdByPage = []; //收集每个分页的congentid；
         //先跑一页，确定总页数，再根据总页数循环
-        let pageOneContentId = getContentId('816',loginUserId,mainUserId);
+        let pageOneContentId = getContentId(shopId, loginUserId, mainUserId, _tk);
         colectContentIdByPage = [...colectContentIdByPage, ...pageOneContentId];
-        console.log('getContentIdPages', getContentIdPages, colectContentIdByPage)
         let i = 2;
-        while (i <= getContentIdPages) {
-          let pageI_ContentId = getContentId(i);
+        while (getContentIdLength > 0) {
+          let pageI_ContentId = getContentId(shopId, loginUserId, mainUserId, _tk, i);
           colectContentIdByPage = [...colectContentIdByPage, ...pageI_ContentId];
-          console.log(i)
           i++;
           util.sleep(Math.random() * (100 - 20) + 20);
         }
       }).catch(() => {
         alert('同步商家信息出错。')
       })
-      $.ajax({
-        url: `${moli_host}/mer/syncPersonalInfo.wb`,
-        type: 'post',
-        headers: {
-          version: '1.1.4',
-          vs: '1',
-          tk: '0000000000000'
-        },
-        async: false,
-        data: res,
-        success(response) {
-          console.log(response);
-        }
-      })
+      // $.ajax({
+      //   url: `${moli_host}/mer/syncPersonalInfo.wb`,
+      //   type: 'post',
+      //   headers: {
+      //     version: '1.1.4',
+      //     vs: '1',
+      //     tk: '0000000000000'
+      //   },
+      //   async: false,
+      //   data: res,
+      //   success(response) {
+      //     console.log(response);
+      //   }
+      // })
 
 
-      getDataFromSycm().then((data) => {
-        let {
-          browsePv,
-          browseUv,
-          contentGuideShopPv,
-          contentGuideShopUv,
-          contentGuideCartByr,
-          contentGuideCartQty,
-          contentGuideCltCnt,
-          contentGuidePayOrdByr,
-          contentGuidePayOrdCnt,
-          contentGuidePayOrdAmt
-        } = data;
-        console.log('get by business:', browsePv, contentGuideShopPv)
-      }).catch((reason) => {
-        console.log('catch:', reason);
-      })
+      // getDataFromSycm().then((data) => {
+      //   let {
+      //     browsePv,
+      //     browseUv,
+      //     contentGuideShopPv,
+      //     contentGuideShopUv,
+      //     contentGuideCartByr,
+      //     contentGuideCartQty,
+      //     contentGuideCltCnt,
+      //     contentGuidePayOrdByr,
+      //     contentGuidePayOrdCnt,
+      //     contentGuidePayOrdAmt
+      //   } = data;
+      //   console.log('get by business:', browsePv, contentGuideShopPv)
+      // }).catch((reason) => {
+      //   console.log('catch:', reason);
+      // })
     }).catch((reason) => {
       alert('请先登录生意参谋。')
       console.log('catch:', reason);
     });
   }
   return
-
-  //检查版本更新
-  if (request.greeting == "checkVersion") {
-    versionObj().then((response) => {
-      // console.log(response, response.result.verSeq, app_verSeq, response.result.verSeq > app_verSeq)
-      //需要更新插件
-      if (response.result.verSeq > app_verSeq) {
-        chrome.tabs.getSelected(null, function(tab) {
-          chrome.tabs.sendRequest(tab.id, {
-            greeting: "checkV",
-            res: response.result,
-          }, function(response) {
-            console.log(response);
-          });
-        });
-        response.result.app_verSeq = app_verSeq;
-        sendResponse(response.result);
-      }
-    })
-  }
-
-
-  //爬数开始 3个if
-  //提前获取config内容
-  if (request.greeting == "triggerConfig") {
-    console.log("正在获取配置数据，请稍候。。。")
-    let config;
-    $.ajax({
-      url: `${moli_host}/spider/config.wb?${request.head}&version=${app_version}&vs=${app_verSeq}`,
-      async: false,
-      success: function(data) {
-        config = data;
-      }
-    })
-    sendResponse(config);
-  }
-
-  //返回已经取得的config结果configRes
-  if (request.greeting == "fetchConfig") {
-    //向前content返回消息
-    sendResponse(configRes);
-  }
-  if (request.greeting == "spider") {
-    //向前content返回消息
-    request.interFaceList.forEach((item, index) => {
-      // console.log(item)
-      let argumentList = [];
-      let postMoliHeadParam = [];
-      //exception begin
-      if (!Array.isArray(item.requestArgs)) {
-        let param = Object.assign({}, {
-          version: app_version,
-          msg: 'request.interFaceList item is not a array - > apiUrl: ' + item.apiUrl
-        })
-        $.ajax({
-          url: `${moli_host}/spider/exception.wb`,
-          type: 'POST',
-          headers: request.head,
-          data: param,
-          success: function(data) {
-            console.log('has error :  +', item.apiUrl)
-          }
-        })
-      } //exception end
-      item.requestArgs.forEach(async function(requestArg, index) {
-        let _headObj = Object.assign({}, request.head);
-        //get请求
-        if (item.apiMethod === "get") {
-          //添加分页的参数到接口请求参数中
-          if (item.pagerArg) {
-            requestArg.getArgs['pageParamField'] = {}
-            requestArg.getArgs['pageParamField'][item.pagerArg.pageSizeArg] = item.pagerArg.defaultPageSize;
-            requestArg.getArgs['pageParamField'][item.pagerArg.pageArg] = 1;
-            requestArg.getArgs['pageParamField']['totalField'] = item.pagerArg.totalField;
-            requestArg.getArgs['pageParamField']['pageArg'] = item.pagerArg.pageArg;
-          }
-          argumentList.push(requestArg.getArgs);
-          //backArgs:
-          if (Array.isArray(requestArg.backArgs)) {
-            requestArg.backArgs.forEach((backArg, ind) => {
-              _headObj[backArg] = requestArg['getArgs'][backArg];
-            })
-          } else {
-            //exception begin
-            //exception end
-          }
-        } else { //post请求
-          //添加分页的参数到接口请求参数中
-          if (item.pagerArg) {
-            requestArg.postArgs['pageParamField'] = {}
-            requestArg.postArgs['pageParamField'][item.pagerArg.pageSizeArg] = item.pagerArg.defaultPageSize;
-            requestArg.postArgs['pageParamField'][item.pagerArg.pageArg] = 1;
-            requestArg.postArgs['pageParamField']['totalField'] = item.pagerArg.totalField;
-            requestArg.postArgs['pageParamField']['pageArg'] = item.pagerArg.pageArg;
-          }
-          argumentList.push(requestArg.postArgs);
-          if (Array.isArray(requestArg.backArgs)) {
-            requestArg.backArgs.forEach((backArg, ind) => {
-              _headObj[backArg] = requestArg['postArgs'][backArg];
-            })
-          } else {
-            //exception begin
-            //exception end
-          }
-        }
-        if (item.contentType) {
-          _headObj["Content-Type"] = item.contentType;
-        }
-        //head参数在这里设置
-        postMoliHeadParam.push(_headObj);
-      })
-      argumentList.forEach((argument, index) => {
-        //暂时默认爬数据都是GET，（此处也不应有post?）
-        var search = "?";
-        let pageObj = null;
-        for (var key in argument) {
-          if (key == "pageParamField") {
-            pageObj = Object.assign({}, argument[key]);
-          } else {
-            if (search === "?") {
-              if (key !== 'syncTime') {
-                search += key + "=" + argument[key];
-              }
-            } else {
-              if (key !== 'syncTime') {
-                search += "&" + key + "=" + argument[key];
-              }
-            }
-          }
-        }
-
-        //爬数
-        let ajaxFn = async function(page = 1, isCallBack = false) {
-          let time = parseInt(Math.random() * (config.max_interval - config.min_interval) + config.min_interval, 10);
-          // util.sleep(time);
-
-          if (pageObj) {
-            if (page > 1) {
-              pageObj[pageObj.pageArg] = page
-            }
-            // if (item.apiUrl.includes('single/detail/result.json'))
-            //   console.log('000000', pageObj)
-            Object.keys(pageObj).forEach((item, id) => {
-              if (item !== "totalField" && item !== "pageArg") {
-                //防止前面一个参数也没有
-                if (search === "?") {
-                  search += item + "=" + pageObj[item];
-                } else {
-                  let jsonList = {};
-                  let str = search.slice(search.indexOf("?") + 1);
-                  let strs = str.split("&")
-                  for (let item of strs) {
-                    jsonList[item.split("=")[0]] = item.split("=")[1];
-                  }
-                  jsonList[item] = pageObj[item]
-
-                  let tempArr = [];
-                  for (let i in jsonList) {
-                    let key = encodeURIComponent(i);
-                    let value = encodeURIComponent(jsonList[i]);
-                    tempArr.push(key + '=' + value);
-                  }
-                  search = "?" + tempArr.join('&');
-                }
-              }
-            })
-          }
-
-          util.sleep(time);
-          $.ajax({
-            url: item.apiUrl + search,
-            type: item.apiMethod || 'GET',
-            // async: false,
-            success: function(data) {
-              var success_flag = item.succFlag.split(":")
-              //保存出错信息
-              if (data[success_flag[0]] == undefined) {
-                let param = Object.assign({}, {
-                  version: app_version,
-                  msg: 'setPlugCookie.js->data[success_flag[0]](line:225)->data:' + JSON.stringify(data) + "   ->success_flag:" + success_flag + "  ->apiUrl:" + item.apiUrl
-                })
-                $.ajax({
-                  url: `${moli_host}/spider/exception.wb`,
-                  type: 'POST',
-                  headers: request.head,
-                  data: param,
-                  success: function(data) {
-                    console.log('has error :  +', item.apiUrl)
-                  }
-                })
-              } else if (data[success_flag[0]].toString() == $.trim(success_flag[1])) {
-                // if (true) { //测试
-                var param = {};
-                //解析出每一个要传递到后台的参数
-                $.each(item.fields, function(_index, field) {
-                  if (field.dataRoot) {
-                    let level = field.dataRoot.split(".");
-                    let _feild = field.fields.split("|");
-                    // let _paramLevel = "";
-                    let _paramObj = null;
-                    //对象
-                    if (field.dataType === "entity") {
-                      for (let len = 0; len < level.length; len++) {
-
-                        _paramObj = _paramObj ? _paramObj[level[len]] : data[level[len]];
-                        // _paramLevel+=`[level[${len}]]`;
-                      }
-                      // console.log('_paramLevel',_paramLevel,data)
-                      $.each(_feild, function(_id, _it) {
-                        let _param = _it.split(":");
-                        // let s=eval(`data${_paramLevel}`)
-                        let s = _paramObj
-                        param[_param[1]] = s[_param[0]]
-                      })
-                    } else if (field.dataType === "list") { //数组
-                      param[field['backArg']] = new Array();
-                      // let _paramLevel = "";
-                      let _paramObj = null;
-
-                      for (let len = 0; len < level.length; len++) {
-                        _paramObj = _paramObj ? _paramObj[level[len]] : data[level[len]];
-                        // _paramLevel+=`[level[${len}]]`;
-                      }
-                      // let finalData = eval(`data${_paramLevel}`);
-                      let finalData = _paramObj;
-
-
-                      if (finalData && finalData.length > 0) {
-                        finalData.forEach((item, index) => {
-                          let objItem = {}
-                          $.each(_feild, function(_id, _it) {
-                            var _param = _it.split(":");
-                            objItem[_param[1]] = item['' + _param[0]]; //字段名有中文
-                          })
-                          param[field['backArg']].push(objItem);
-                        })
-                      }
-                    } else {
-                      param[field['dataField']] = data[field['dataField']]
-                    }
-                  } else {
-                    param[field['dataField']] = data[field['dataField']]
-                  }
-                })
-
-                //触发回填数据
-                let _par = param
-                if (postMoliHeadParam[index]['Content-Type']) {
-                  _par = JSON.stringify(_par)
-                }
-                if (Object.keys(_par).length) {
-                  // util.sleep(parseInt(Math.random() * (2500 - 100) + 120, 10);)
-                  $.ajax({
-                    // url: request.url,
-                    url: item.serviceUrl,
-                    type: 'POST',
-                    async: true,
-                    tiem: 2300,
-                    headers: postMoliHeadParam[index],
-                    data: _par,
-                    success: function(res) {
-                      // console.log(res);
-                    }
-                  })
-                }
-
-                if (pageObj) {
-                  let paramArr = pageObj.totalField.split('.');
-                  let _paramObj = null;
-                  for (let i = 0; i < paramArr.length; i++) {
-                    _paramObj = _paramObj ? _paramObj[paramArr[i]] : data[paramArr[i]];
-                  }
-                  let totalItem = _paramObj;
-                  let totalPage = parseInt((parseInt(totalItem) + parseInt(pageObj.pageSize) - 1) / parseInt(pageObj.pageSize));
-                  if (!isCallBack) {
-                    for (let t = 2; t <= totalPage; t++) {
-                      ajaxFn(t, true)
-                    }
-                  }
-                }
-
-              }
-            }
-          })
-        }
-        ajaxFn();
-        //爬数结束
-      })
-    })
-  }
-
-  //爬数结束
 
 });
